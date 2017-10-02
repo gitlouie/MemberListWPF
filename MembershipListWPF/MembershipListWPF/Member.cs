e9862fd3d857f3d303dd31ab7397ad20e712810b
gitlouie <louietorres93@gmail.com>
2017-10-01 22:32:18 -0700

    first class

--- MembershipListWPF/MembershipListWPF/Member.cs
+++ MembershipListWPF/MembershipListWPF/Member.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace MembershipListWPF
 {
-    class Member
+    class Member 
     {
+        private string firstName, lastName, email;
+
+        public Member(string firstName, string lastName, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+        }
+
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set 
+            {
+                if (firstName.Length > 25) firstName = value;
+                else throw (new DivideByZeroException("No more than 25 characters please!"));
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (lastName.Length > 25) lastName = value;
+                else throw (new DivideByZeroException("No more than 25 characters please!"));
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (email.Length > 25) email = value;
+                else throw (new DivideByZeroException("No more than 25 characters please!"));
+            }
+        }
+
+        public string GetDisplayText()
+        {
+            return firstName + " " + lastName + " " + email;
+        }
+        
     }
+
+    
 }
