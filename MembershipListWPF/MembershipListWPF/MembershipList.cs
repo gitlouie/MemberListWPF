19e8e6bed6e9b3aeaeea1a91c83b017cd0fb46f4
gitlouie <louietorres93@gmail.com>
2017-10-03 22:02:09 -0700

    yup

---
+++ MembershipListWPF/MembershipListWPF/MembershipList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembershipListWPF
+{
+    class MembershipList
+    {
+        private List<Member> memList;
+        
+        public MembershipList ()
+        {
+            memList = new List<Member>();
+        }
+
+        //Adds a membership to the list
+        public void Add(Member m)
+        {
+            memList.Add(m);
+        }
+
+        public void Remove(Member m)
+        {
+            memList.Remove(m);
+        }
+
+
+    }
+}
